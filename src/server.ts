64d622f2733743d2bb6de32f01aaaa9c7612ae65
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-10-02 15:26:53 -0300

    feat: post and get route and query params

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,28 @@
 import express from 'express';
+import { execArgv } from 'process';
 
 const PORT = 3333;
 
 const app = express();
+app.use(express.json());
+
+app.get('/route/:id/:user', (req, res) => {
+  console.log(req.params);
+  res.json(req.params);
+});
+
+app.get('/query', (req, res) => {
+  console.log(req.query);
+  res.json(req.query);
+});
+
+app.post('/products', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
