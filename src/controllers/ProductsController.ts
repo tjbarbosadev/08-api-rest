5dc4ab55e02cf3a8a014341f301086a72e7f7e2e
Thiago Barbosa <tjbarbosa.dev@gmail.com>
2025-10-03 20:25:20 -0300

    feat: configuring required message errors

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -19,8 +19,8 @@ export class ProductsController {
 
   create(req: Request, res: Response) {
     const bodySchema = z.object({
-      name: z.string(),
-      price: z.number(),
+      name: z.string({ required_error: 'Name is required!' }),
+      price: z.number({ required_error: 'Price is required!' }),
     });
 
     const { name, price } = bodySchema.parse(req.body);
